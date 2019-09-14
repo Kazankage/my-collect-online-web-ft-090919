669291bc47cb7458d4551635fa0cf08bf1f95fd7
Randel Jiao <randeljiao@gmail.com>
2019-09-14 20:00:10 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,9 @@
-
-
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
 end
